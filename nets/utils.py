431e52275c3322b40b09a48884464dfb8249fcb9
QiYao <qy@stu.scu.edu.cn>
2018-02-14 19:32:30 +0800

    move avg_grad to trainer.py

--- nets/utils.py
+++ nets/utils.py
@@ -1,23 +1,6 @@
 import tensorflow as tf
 
 
-def average_gradients(tower_grads):
-    average_grads = []
-    for grad_and_vars in zip(*tower_grads):
-        grads = []
-        for g, _ in grad_and_vars:
-            if g is None:
-                continue
-            expanded_g = tf.expand_dims(g, 0)
-            grads.append(expanded_g)
-        grad = tf.concat(axis=0, values=grads)
-        grad = tf.reduce_mean(grad, 0)
-        v = grad_and_vars[0][1]
-        grad_and_var = (grad, v)
-        average_grads.append(grad_and_var)
-    return average_grads
-
-
 def read_from_tfrecord(tfrecord_file_queue, flags):
     reader = tf.TFRecordReader()
     _, tfrecord_serialized = reader.read(tfrecord_file_queue)
